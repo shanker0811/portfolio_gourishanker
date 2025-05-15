a53abe480dea73b9dc79417c7ef22795e8b87687
shanker1997 <gourishanker697@gmail.com>
2025-05-15 07:22:27 +0530

    feat: animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import react from 'react'
+import Lottie from 'lottie-react'
+import animationData from '../assets/lootiefiles/developeranimaton.json'
+import workingdevanima from '../assets/lootiefiles/Webdeveloper.json'
 
 function App() {
 
   return (
     <>
+    <div className='flex flex-row align-items-center justify-content-evenly p-3'>
+      <div className='bg-white'>
+        <p className='m-0 p-2'> The data of developer</p>
+
+      </div>
+
+    <div className="w-60 h-70">
+      <Lottie animationData={animationData} loop={true} />
+    </div>
+    </div>
+    <div className='flex flex-row align-items-center justify-content-evenly p-3'>
+
+    <div className="w-80 h-70">
+      <Lottie animationData={workingdevanima} loop={true} />
+    </div>
+    </div>
  <div className="bgprimary">
   Tailwind is working!
 </div>
