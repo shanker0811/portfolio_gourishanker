3e9adc368ede2edc73a412ec7767e5b14fdc9646
shanker1997 <gourishanker697@gmail.com>
2025-05-15 06:22:11 +0530

    feat: tailwind configuration

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import react from 'react'
+
+function App() {
+
+  return (
+    <>
+ <div className="bgprimary">
+  Tailwind is working!
+</div>
+<div className="bg-blue-400 text-white p-4">
+  Bit Brown background
+</div>
+
+<div className="bg-blue-400 text-black p-4">
+  Light gray background
+</div>
+    </>
+  )
+}
+
+export default App
