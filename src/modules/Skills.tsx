43bf34c24ed8bdfcd8d61c3c9424bb1c5f9ac513
shanker1997 <gourishanker697@gmail.com>
2025-05-17 22:32:53 +0530

    skills cards changes

--- src/modules/Skills.tsx
+++ src/modules/Skills.tsx
@@ -1,13 +1,110 @@
 function Skills() {
+  let skillsArray = [
+    {
+      technology: "Javascript",
+      experience: "3yrs",
+      icon: (
+        <i
+          className="fa-brands fa-js"
+          style={{ color: "#f7df1e", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+    {
+      technology: "React.js",
+      experience: "2.5yrs",
+      icon: (
+        <i
+          className="fa-brands fa-react"
+          style={{ color: "#61dafb", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+    {
+      technology: "HTML",
+      experience: "3yrs",
+      icon: (
+        <i
+          className="fa-brands fa-html5"
+          style={{ color: "#e44d26", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+    {
+      technology: "CSS",
+      experience: "3yrs",
+      icon: (
+        <i
+          className="fa-brands fa-css3-alt"
+          style={{ color: "#264de4", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+
+    {
+      technology: "jQuery",
+      experience: "2yrs",
+      icon: (
+        <i
+          className="fa-solid fa-code"
+          style={{ color: "#0769ad", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+    {
+      technology: "TypeScript",
+      experience: "3yrs",
+      icon: (
+        <i
+          className="fa-solid fa-code"
+          style={{ color: "#3178c6", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+
+    {
+      technology: "Node.js",
+      experience: "1yr",
+      icon: (
+        <i
+          className="fa-brands fa-node-js"
+          style={{ color: "#83cd29", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+
+    {
+      technology: "MongoDB",
+      experience: "1yr",
+      icon: (
+        <i
+          className="fa-solid fa-database"
+          style={{ color: "#47a248", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+    {
+      technology: "Express.js",
+      experience: "1yr",
+      icon: (
+        <i
+          className="fa-solid fa-code"
+          style={{ color: "#000000", fontSize: "24px" }}
+        ></i>
+      ),
+    },
+  ];
+
   return (
-    <section id="skills" className="container py-5">
+    <section id="skills" className="container-fluid py-5">
       <h2 className="text-center mb-4">Skills</h2>
       <div className="row justify-content-center">
-        {["JavaScript", "HTML", "CSS", "React"].map((skill, i) => (
+        {skillsArray.map((skill, i) => (
           <div className="col-md-3 col-6 mb-3 text-center" key={i}>
-            <div className="border rounded p-3 bg-white">
-              <h5>{skill}</h5>
-              <p>3+ Years</p>
+            <div className="skill-card">
+              <>{skill?.icon}</>
+              <h5>{skill?.technology}</h5>
+              <p>{skill?.experience}</p>
             </div>
           </div>
         ))}
